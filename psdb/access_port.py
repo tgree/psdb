57560522fb9c11a9683d6df40aefd94dce2b50ff
Terry Greeniaus <terrygreeniaus@gmail.com>
2019-10-29 18:59:39 -0600

    access_port.py: Add code to manually perform AP accesses for probes that can't always offload them to firmware.

--- psdb/access_port.py
+++ psdb/access_port.py
@@ -148,6 +148,198 @@ class MemAP(AP):
     def _write_bd(self, val, index):
         self.db.write_ap_reg(self.ap_num, 0x10 + 4*index, val)
 
+    def _read_generic(self, addr, size):
+        flag = MemAP.SIZE_TABLE[size]
+        assert self.flags & flag
+        if self.csw_size != size:
+            self._write_csw(self.csw_base | size)
+        if self.tar != addr:
+            self._write_tar(addr)
+        v = self._read_drw()
+        if self.csw_ainc == 1:
+            self.tar += (1 << size)
+        return ((v >> 8*(addr % 4)) & MemAP.MASK_TABLE[size])
+
+    def _read_8(self, addr):
+        return self._read_generic(addr, 0)
+
+    def _read_16(self, addr):
+        return self._read_generic(addr, 1)
+
+    def _read_32(self, addr):
+        if (self.tar & 0xFFFFFFF0) == (addr & 0xFFFFFFF0):
+            return self._read_bd((addr & 0xF) // 4)
+        return self._read_generic(addr, 2)
+
+    def _read_mem_page(self, addr, n):
+        if not n:
+            return
+
+        assert (addr & 0xFFFFFC00) == ((addr + n - 1) & 0xFFFFFC00)
+        if self.tar != addr:
+            self._write_tar(addr)
+
+        mem = ''
+        if n >= 1 and (addr % 2):
+            assert self.flags & MemAP.FLAG_SIZE_8
+            self._write_csw(self.csw_base | (1<<4))
+            v = (self._read_drw() >> 8*(addr % 4))
+            mem  += chr((v >> 0) & 0xFF)
+            addr += 1
+            n    -= 1
+
+        if n >= 2 and (addr % 4):
+            assert self.flags & MemAP.FLAG_SIZE_16
+            self._write_csw(self.csw_base | (1<<4) | 1)
+            v = (self._read_drw() >> 8*(addr % 4))
+            mem  += chr((v >> 0) & 0xFF)
+            mem  += chr((v >> 8) & 0xFF)
+            addr += 2
+            n    -= 2
+
+        if n >= 4:
+            assert (addr % 4) == 0
+            assert self.flags & MemAP.FLAG_SIZE_32
+            self._write_csw(self.csw_base | (1<<4) | 2)
+            while n >= 4:
+                v = (self._read_drw() >> 8*(addr % 4))
+                mem  += chr((v >>  0) & 0xFF)
+                mem  += chr((v >>  8) & 0xFF)
+                mem  += chr((v >> 16) & 0xFF)
+                mem  += chr((v >> 24) & 0xFF)
+                addr += 4
+                n    -= 4
+
+        if n >= 2:
+            assert (addr % 2) == 0
+            assert self.flags & MemAP.FLAG_SIZE_16
+            self._write_csw(self.csw_base | (1<<4) | 1)
+            v = (self._read_drw() >> 8*(addr % 4))
+            mem  += chr((v >> 0) & 0xFF)
+            mem  += chr((v >> 8) & 0xFF)
+            addr += 2
+            n    -= 2
+
+        if n == 1:
+            assert self.flags & MemAP.FLAG_SIZE_8
+            self._write_csw(self.csw_base | (1<<4))
+            v = (self._read_drw() >> 8*(addr % 4))
+            mem  += chr((v >> 0) & 0xFF)
+            addr += 1
+            n    -= 1
+
+        self._write_csw(self.csw_base | 2)
+        self._write_tar(addr)
+        return mem
+
+    def _read_bulk(self, addr, n):
+        mem = ''
+        while n:
+            count = min(n, 0x400 - (addr & 0x3FF))
+            mem  += self._read_mem_page(addr, count)
+            addr += count
+            n    -= count
+        return mem
+
+    def _write_generic(self, v, addr, size):
+        flag = MemAP.SIZE_TABLE[size]
+        assert self.flags & flag
+        if self.csw_size != size:
+            self._write_csw(self.csw_base | size)
+        if self.tar != addr:
+            self._write_tar(addr)
+        v = ((v & MemAP.MASK_TABLE[size]) << 8*(addr % 4))
+        self._write_drw(v)
+        if self.csw_ainc == 1:
+            self.tar += (1 << size)
+
+    def _write_8(self, v, addr):
+        self._write_generic(v, addr, 0)
+
+    def _write_16(self, v, addr):
+        self._write_generic(v, addr, 1)
+
+    def _write_32(self, v, addr):
+        if (self.tar & 0xFFFFFFF0) == (addr & 0xFFFFFFF0):
+            self._write_bd(v, (addr & 0xF) // 4)
+        else:
+            self._write_generic(v, addr, 2)
+
+    def _write_mem_page(self, data, addr):
+        if not data:
+            return
+
+        n = len(data)
+        assert (addr & 0xFFFFFC00) == ((addr + n - 1) & 0xFFFFFC00)
+        if self.tar != addr:
+            self._write_tar(addr)
+
+        offset = 0
+
+        if n >= 1 and (addr % 2):
+            assert self.flags & MemAP.FLAG_SIZE_8
+            self._write_csw(self.csw_base | (1<<4))
+            self._write_drw(struct.unpack_from('B', data, offset)[0]
+                            << 8*(addr % 4))
+            addr   += 1
+            offset += 1
+            n      -= 1
+
+        if n >= 2 and (addr % 4):
+            assert self.flags & MemAP.FLAG_SIZE_16
+            self._write_csw(self.csw_base | (1<<4) | 1)
+            self._write_drw(struct.unpack_from('<H', data, offset)[0]
+                            << 8*(addr % 4))
+            addr   += 2
+            offset += 2
+            n      -= 2
+
+        if n >= 4:
+            assert (addr % 4) == 0
+            assert self.flags & MemAP.FLAG_SIZE_32
+            self._write_csw(self.csw_base | (1<<4) | 2)
+            while n >= 4:
+                self._write_drw(struct.unpack_from('<I', data, offset)[0])
+                addr   += 4
+                offset += 4
+                n      -= 4
+
+        if n >= 2:
+            assert (addr % 2) == 0
+            assert self.flags & MemAP.FLAG_SIZE_16
+            self._write_csw(self.csw_base | (1<<4) | 1)
+            self._write_drw(struct.unpack_from('<H', data, offset)[0]
+                            << 8*(addr % 4))
+            addr   += 2
+            offset += 2
+            n      -= 2
+
+        if n == 1:
+            assert self.flags & MemAP.FLAG_SIZE_8
+            self._write_csw(self.csw_base | (1<<4))
+            self._write_drw(struct.unpack_from('B', data, offset)[0]
+                           << 8*(addr % 4))
+            addr   += 1
+            offset += 1
+            n      -= 1
+
+        self._write_csw(self.csw_base | 2)
+        if addr & 0x3FF:
+            self.tar = addr
+        else:
+            self._write_tar(addr)
+
+    def _write_bulk(self, data, addr):
+        offset = 0
+        n      = len(data)
+        while n:
+            count = min(n, 0x400 - (addr & 0x3FF))
+            self._write_mem_page(data[offset:offset + count], addr)
+
+            addr   += count
+            offset += count
+            n      -= count
+
 
 class AHBAP(MemAP):
     '''
