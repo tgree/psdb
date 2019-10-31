39ae6fe1ca2486e95894101f3d87dd4aea5198f6
Terry Greeniaus <terrygreeniaus@gmail.com>
2019-10-30 19:47:07 -0600

    stm32g4: Add support for STM32G4 MCU and FLASH controller.

--- psdb/targets/__init__.py
+++ psdb/targets/__init__.py
@@ -3,10 +3,12 @@ from .target import (Target, MemRegion)
 from .device import Reg, MemDevice
 from . import msp432
 from . import stm32h7
+from . import stm32g4
 
 
 TARGETS = [msp432.MSP432P401,
            stm32h7.STM32H7,
+           stm32g4.STM32G4,
            ]
 
 def probe(db):
