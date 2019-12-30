16537d2758e638a8e24acea59681a10b8a4d94c5
Terry Greeniaus <terrygreeniaus@gmail.com>
2019-12-30 14:51:33 -0700

    stm32g4: Fix 16-bit timer definitions.
    It turns out there are a few variants of the 16-bit timers that each
    have a few different capabilities and registers.  This commit sorts them
    all out.

---
+++ psdb/targets/stm32g4/general_purpose_timer_16x2.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2019 Phase Advanced Sensor Systems, Inc.
+from ..device import Device, Reg32, Reg32W
+
+
+class GPT16x2(Device):
+    '''
+    Driver for the General-purpose Timer device (GPT) supporting only 16-bit
+    counters and two capture/compare registers.
+    '''
+    REGS = [Reg32 ('CR1',    0x000),
+            Reg32 ('CR2',    0x004),
+            Reg32 ('SMCR',   0x008),
+            Reg32 ('DIER',   0x00C),
+            Reg32 ('SR',     0x010),
+            Reg32W('EGR',    0x014),
+            Reg32 ('CCMR1',  0x018),
+            Reg32 ('CCER',   0x020),
+            Reg32 ('CNT',    0x024),
+            Reg32 ('PSC',    0x028),
+            Reg32 ('ARR',    0x02C),
+            Reg32 ('RCR',    0x030),
+            Reg32 ('CCR1',   0x034),
+            Reg32 ('CCR2',   0x038),
+            Reg32 ('BDTR',   0x044),
+            Reg32 ('DTR2',   0x054),
+            Reg32 ('TISEL',  0x05C),
+            Reg32 ('AF1',    0x060),
+            Reg32 ('AF2',    0x064),
+            Reg32 ('DCR',    0x3DC),
+            Reg32 ('DMAR',   0x3E0),
+            ]
+
+    def __init__(self, target, name, addr):
+        super(GPT16x2, self).__init__(target, addr, name, GPT16x2.REGS)
