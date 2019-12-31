0f384b5a6de9a1d16e499b26b307a2f9e487e8f3
Terry Greeniaus <terrygreeniaus@gmail.com>
2019-12-31 15:56:50 -0700

    stm32h7: Add a few more checks when probing.

--- psdb/targets/stm32h7/stm32h7.py
+++ psdb/targets/stm32h7/stm32h7.py
@@ -33,14 +33,25 @@ class STM32H7(Target):
         if not isinstance(ap, psdb.access_port.AHBAP):
             return None
 
+        # APSEL 2 should be an APB AP.
+        if not isinstance(db.aps[2], psdb.access_port.APBAP):
+            return None
+
         # Identify the STM32H7 through the base component's CIDR/PIDR
         # registers.
-        c = db.aps[0].base_component
-        if not c or c.cidr != 0xB105100D or c.pidr != 0x00000000000A0450:
-            return None
+        for ap in (db.aps[0], db.aps[2]):
+            c = ap.base_component
+            if not c or c.cidr != 0xB105100D or c.pidr != 0x00000000000A0450:
+                return None
 
         # There should be exactly one CPU.
         if len(db.cpus) != 1:
+            return None
+
+        # Finally, we can match on the DBGMCU IDC value.
+        rt1 = db.aps[2].base_component.find_components_by_type(STM32H7ROMTable1)
+        assert len(rt1) == 1
+        if (rt1[0].read_dbgmcu_idc() & 0x00000FFF) != 0x450:
             return None
 
         return STM32H7(db)
