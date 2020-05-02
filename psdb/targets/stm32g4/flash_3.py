8a3f7a85e9729529b127a7c0d4d8cb59791e4d9d
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-05-01 19:03:38 -0600

    stm32g4: Refactor flash and device code to prepare for non-category 3 devices.

--- psdb/targets/stm32g4/flash_3.py
+++ psdb/targets/stm32g4/flash_3.py
@@ -1,35 +1,16 @@
 # Copyright (c) 2019 Phase Advanced Sensor Systems, Inc.
-from ..device import Device, Reg32, Reg32W
-from ..flash import Flash
+from ..device import Reg32, Reg32W
+from .flash_base import FLASH_Base
 
 
-def block_in_region(addr, size, region_base, region_len):
-    return ((region_base <= addr) and
-            (addr + size <= region_base + region_len))
-
-
-def write_in_region(addr, data, region_base, region_len):
-    return block_in_region(addr, len(data), region_base, region_len)
-
-
-class UnlockedContextManager(object):
-    def __init__(self, flash):
-        self.flash = flash
-
-    def __enter__(self):
-        v = self.flash._read_cr()
-        if v & (1<<31):
-            self.flash._write_keyr(0x45670123)
-            self.flash._write_keyr(0xCDEF89AB)
-
-    def __exit__(self, type, value, traceback):
-        v = self.flash._read_cr()
-        self.flash._write_cr(v | (1<<31))
-
-
-class FLASH(Device, Flash):
+class FLASH_3(FLASH_Base):
     '''
-    Driver for the FLASH device on the STM32G4 series of MCUs.
+    Driver for the FLASH category 3 device on the STM32G4 series of MCUs:
+        STM32G471
+        STM32G473
+        STM32G474
+        STM32G483
+        STM32G484
     '''
     REGS = [Reg32 ('ACR',           0x000),
             Reg32 ('PDKEYR',        0x004),
@@ -74,50 +55,22 @@ class FLASH(Device, Flash):
 
     def __init__(self, target, ap, name, dev_base, mem_base, max_write_freq,
                  otp_base, otp_len):
-        Device.__init__(self, target, ap, dev_base, name, FLASH.REGS)
-        self.otp_base = otp_base
-        self.otp_len  = otp_len
-
-        optr = self._read_optr()
+        optr = ap.read_32(dev_base + 0x20)
+        print('OPTR: 0x%08X' % optr)
         if optr == 0:
             raise Exception('Unexpected OPTR=0, debug clocks may be disabled; '
                             'try using --srst')
         sector_size = 2048 if optr & (1<<22) else 4096
-        Flash.__init__(self, mem_base, sector_size,
-                       target.flash_size // sector_size)
-        self.max_write_freq = max_write_freq
 
-    def _flash_unlocked(self):
-        return UnlockedContextManager(self)
-
-    def _clear_errors(self):
-        self._write_sr(self._read_sr())
-
-    def _check_errors(self):
-        v = self._read_sr()
-        if v & 0x0000C3F8:
-            raise Exception('Flash operation failed, FLASH_SR=0x%08X' % v)
-
-    def _wait_bsy_clear(self):
-        while self._read_sr() & (1 << 16):
-            pass
-
-    def set_swd_freq_write(self, verbose=True):
-        f = self.target.db.set_tck_freq(self.max_write_freq)
-        if verbose:
-            print('Set SWD frequency to %.3f MHz' % (f/1.e6))
-
-    def set_swd_freq_read(self, verbose=True):
-        f = self.target.set_max_tck_freq()
-        if verbose:
-            print('Set SWD frequency to %.3f MHz' % (f/1.e6))
+        FLASH_Base.__init__(self, FLASH_3.REGS, sector_size, target, ap, name,
+                            dev_base, mem_base, max_write_freq, otp_base,
+                            otp_len)
 
     def erase_sector(self, n, verbose=True):
         '''
         Erases the nth sector in flash.
-        The sector is verified to be erased before returning.  This checks if
-        the flash banks are swapped and erases the appropriate sector if it
-        needs to reverse the numbers.
+        This checks if the flash banks are swapped and erases the appropriate
+        sector if it needs to reverse the numbers.
         '''
         assert 0 <= n and n < self.nsectors
 
@@ -144,67 +97,7 @@ class FLASH(Device, Flash):
             self._write_cr((1 << 16) | (n << 3) | (1 << 1) | bker)
             self._wait_bsy_clear()
             self._check_errors()
-
-    def read(self, addr, length):
-        '''
-        Reads a region from the flash.
-        '''
-        return self.ap.read_bulk(addr, length)
-
-    def write(self, addr, data, verbose=True):
-        '''
-        Writes 8-byte lines of data to the flash.  The address must be
-        8-byte aligned and the data to write must be a multiple of 8 bytes in
-        length.
-
-        The target region to be written must be in the erased state.
-        '''
-        assert self.target.is_halted()
-        if not data:
-            return
-        assert len(data) % 8 == 0
-        assert addr % 8 == 0
-        assert (write_in_region(addr, data, self.mem_base, self.flash_size) or
-                write_in_region(addr, data, self.otp_base, self.otp_len))
-
-        if verbose:
-            print('Flashing region [0x%08X - 0x%08X]...' % (
-                    addr, addr + len(data) - 1))
-
-        with self._flash_unlocked():
-            self._clear_errors()
-            self._write_cr(1 << 0)
-            self.ap.write_bulk(data, addr)
-            self._wait_bsy_clear()
-            self._check_errors()
-
-    def read_otp(self, offset, size):
-        '''
-        Reads a block of one-time-programmable memory.
-        '''
-        assert offset + size <= self.otp_len
-        return self.ap.read_bulk(self.otp_base + offset, size)
-
-    def is_otp_writeable(self, offset, size, verbose=True):
-        '''
-        Determines if the selected region of one-time-programmable memory is
-        still writeable.
-        '''
-        return self.read_otp(offset, size) == (b'\xFF'*size)
-
-    def write_otp(self, offset, data, verbose=True):
-        '''
-        Writes 8-byte lines of data to the one-time-programmable area in flash.
-        The address must be 8-byte aligned and the data to write must be a
-        multiple of 8 bytes in length.
-
-        The target region to be written must be in the erased state (every 8-
-        byte double-word must be exactly 0xFFFFFFFFFFFFFFFF - if any of the 64
-        bits has already been written, the entire double-word is no longer
-        writeable).
-        '''
-        assert self.is_otp_writeable(offset, len(data))
-        self.write(self.otp_base + offset, data)
+            self._write_cr(0)
 
     def swap_banks_and_reset(self):
         '''
