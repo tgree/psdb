069d6eb153c3275c348f0087c523289fda568a0c
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-05-02 01:24:35 -0600

    ipc/ws_client.py: Add print_ws_version() method.

--- psdb/targets/stm32wb55/ipc/ws_client.py
+++ psdb/targets/stm32wb55/ipc/ws_client.py
@@ -27,3 +27,35 @@ class WSClient(object):
                 from .fus_client import FUSClient
                 time.sleep(0.1)
                 return self.ipc._start_firmware(FUSClient)
+
+    def print_ws_version(self):
+        version = self.get_fus_version()
+        print('FUS version: 0x%08X (%u.%u.%u)' % (version,
+                                                  ((version >> 24) & 0xFF),
+                                                  ((version >> 16) & 0xFF),
+                                                  ((version >>  8) & 0xFF)))
+        version = self.get_ws_version()
+        print(' WS version: 0x%08X (%u.%u.%u)' % (version,
+                                                  ((version >> 24) & 0xFF),
+                                                  ((version >> 16) & 0xFF),
+                                                  ((version >>  8) & 0xFF)))
+        info = self.get_ws_info()
+        print('    WS info: 0x%08X' % info)
+
+    def get_fus_version(self):
+        '''
+        Returns the FUS version field.
+        '''
+        return self.ipc.ap.read_32(self.ipc.mailbox.dit_addr + 4)
+
+    def get_ws_version(self):
+        '''
+        Returns the WS firmware version field.
+        '''
+        return self.ipc.ap.read_32(self.ipc.mailbox.dit_addr + 16)
+
+    def get_ws_info(self):
+        '''
+        Returns the WS firmware info stack field.
+        '''
+        return self.ipc.ap.read_32(self.ipc.mailbox.dit_addr + 24)
