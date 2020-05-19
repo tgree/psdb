8b7d39f35847764c691b0f373b91f01c90a32561
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-05-19 17:13:09 -0600

    ipc/packet.py: Add types for BLE packets.

--- psdb/targets/stm32wb55/ipc/packet.py
+++ psdb/targets/stm32wb55/ipc/packet.py
@@ -12,6 +12,10 @@ of the linked-list pointers doesn't limit the size of the rest of the packet.
 import struct
 
 
+def hexify(data):
+    return ' '.join('%02X' % v for v in data)
+
+
 def write_sys_command(ap, addr, opcode, payload, next_ptr=0, prev_ptr=0):
     '''
     Command data type for the system channel.  The command is typically written
@@ -121,3 +125,141 @@ class SysEvent(object):
             % (self.subevtcode, self.payload))
 
 
+def write_ble_command(ap, addr, opcode, payload, next_ptr=0, prev_ptr=0):
+    '''
+    Command data type for the BLE channel.  The command is typically written to
+    the BLE Table BLE Command Buffer and is not linked on a list.
+
+        +-------------------------------------------------------------------+
+        |                             NEXT PTR                              |
+        +-------------------------------------------------------------------+
+        |                             PREV PTR                              |
+        +----------------+---------------------------------+----------------+
+        |  TYPE = 0x01   |            CMDCODE              |     PAYLEN     | 
+        +----------------+---------------------------------+----------------+
+        |   PAYLOAD...
+        +-----------------
+    '''
+    data = struct.pack('<LLBHB',
+                       next_ptr,
+                       prev_ptr,
+                       0x01,
+                       opcode,
+                       len(payload)) + payload
+    ap.write_bulk(data, addr)
+
+
+class BLECommandStatus(object):
+    '''
+    Asynchronous command status event on the BLE event channel.
+
+        +-------------------------------------------------------------------+
+        |                             NEXT PTR                              |
+        +-------------------------------------------------------------------+
+        |                             PREV PTR                              |
+        +----------------+----------------+----------------+----------------+
+        |  TYPE = 0x04   | EVTCODE = 0x0F |   PAYLEN = 4   |     STATUS     |
+        +----------------+----------------+----------------+----------------+
+        |    NUMCMD      |             CMDCODE             |
+        +----------------+---------------------------------+
+
+    The NUMCMD field, when printed by some ST dump code, is listed as "numhci".
+    Elsewhere, it is noted that this field indicates the number of commands
+    that the BLE is willing to accept at once; typically this value is 1.
+
+    This type of event is a Command Response and as such it should not be freed
+    back to the MM.
+    '''
+    def __init__(self, ap, addr, data):
+        self.addr = addr
+
+        (plen,
+         self.status,
+         self.numcmd,
+         self.cmdcode) = struct.unpack_from('BBBH', data, 2)
+        assert plen == 4
+
+    def __repr__(self):
+        return ('BLECommandStatus({status: 0x%02X, numcmd: 0x%02X, '
+                'cmdcode: 0x%04X})' % (self.status, self.numcmd, self.cmdcode))
+
+
+class BLECommandComplete(object):
+    '''
+    Asynchronous command complete event on the BLE event channel.
+
+        +-------------------------------------------------------------------+
+        |                             NEXT PTR                              |
+        +-------------------------------------------------------------------+
+        |                             PREV PTR                              |
+        +----------------+----------------+----------------+----------------+
+        |  TYPE = 0x04   | EVTCODE = 0x0E |    PAYLEN+3    |     NUMCMD     |
+        +----------------+----------------+----------------+----------------+
+        |             CMDCODE             |   PAYLOAD...
+        +---------------------------------+-----------------
+
+    The NUMCMD field, when printed by some ST dump code, is listed as "numhci".
+    Elsewhere, it is noted that this field indicates the number of commands
+    that the BLE is willing to accept at once; typically this value is 1.
+
+    This type of event is a Command Response and as such it should not be freed
+    back to the MM.  In fact, this packet occupies the BLE Command Buffer used
+    to send a command, so it would really break things to try and MM-free it.
+    '''
+    def __init__(self, ap, addr, data):
+        self.addr = addr
+
+        (plen,
+         self.numcmd,
+         self.cmdcode) = struct.unpack_from('BBH', data, 2)
+        assert plen >= 3
+        self.payload = ap.read_bulk(addr + 14, plen - 3)
+
+    def __repr__(self):
+        return ('BLECommandComplete({numcmd: 0x%02X, cmdcode: 0x%04X, '
+                'payload: [%s]})' % (self.numcmd, self.cmdcode,
+                                     hexify(self.payload)))
+
+
+class BLE_VS_Event(object):
+    '''
+    Some sort of asynch event which seems to have the same structure as a
+    system channel event.
+
+        +-------------------------------------------------------------------+
+        |                             NEXT PTR                              |
+        +-------------------------------------------------------------------+
+        |                             PREV PTR                              |
+        +----------------+----------------+----------------+----------------+
+        |  TYPE = 0x04   | EVTCODE = 0xFF |    PAYLEN+2    |  SUBEVTCODE   ...
+        +----------------+----------------+----------------+----------------+
+       ... SUBEVTCODE    |   PAYLOAD...
+        +----------------+-----------------
+
+    Since this type of event is not a Command Response, it does need to be
+    freed back to the MM.
+    '''
+    def __init__(self, ap, addr, data):
+        self.addr = addr
+
+        (plen,
+         self.subevtcode) = struct.unpack_from('BH', data, 2)
+        assert plen >= 2
+        self.payload = ap.read_bulk(addr + 13, plen - 2)
+
+    def __repr__(self):
+        return ('BLE_VS_Event({subevtcode: 0x%04X, payload: %s})'
+                % (self.subevtcode, self.payload))
+
+
+def make_ble_event(ap, addr):
+    data = ap.read_bulk(addr + 8, 8)
+    assert data[0] == 0x04
+    assert data[1] in (0x0E, 0x0F, 0xFF)
+
+    if data[1] == 0x0E:
+        return BLECommandComplete(ap, addr, data)
+    elif data[1] == 0x0F:
+        return BLECommandStatus(ap, addr, data)
+    elif data[1] == 0xFF:
+        return BLE_VS_Event(ap, addr, data)
