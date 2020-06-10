1456a384a48b58d24df0b38f451f4256e3f62de1
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-06-10 03:14:51 -0600

    ipc/gatt.py: Add Service.add_char() method.

--- psdb/targets/stm32wb55/ipc/gatt.py
+++ psdb/targets/stm32wb55/ipc/gatt.py
@@ -6,6 +6,12 @@ class Service(object):
         self.ble_channel    = ble_channel
         self.service_handle = service_handle
 
+    def add_char(self, u, char_max_len, char_properties, security_permissions=0,
+                 gatt_evt_mask=0, enc_key_size=16, is_variable=False):
+        return self.ble_channel.aci_gatt_add_char(
+                self.service_handle, u, char_max_len, char_properties,
+                security_permissions, gatt_evt_mask, enc_key_size, is_variable)
+
     def update_char_value(self, char_handle, char_value, val_offset=0):
         self.ble_channel.aci_gatt_update_char_value(self.service_handle,
                                                     char_handle, char_value,
