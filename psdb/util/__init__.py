e915414bde9bfb855a3aa0534db473233fe27d50
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-06-12 02:26:20 -0600

    psdb/util: Refactor hexify() out of ipc.packet.

--- psdb/util/__init__.py
+++ psdb/util/__init__.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2020 Phase Advanced Sensor Systems, Inc.
 from .prange import piter, prange
+from .hexify import hexify
 
 
-__all__ = ['piter',
+__all__ = ['hexify',
+           'piter',
            'prange',
            ]
---
+++ psdb/util/hexify.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2020 Phase Advanced Sensor Systems, Inc.
+
+
+def hexify(data):
+    return ' '.join('%02X' % v for v in data)
