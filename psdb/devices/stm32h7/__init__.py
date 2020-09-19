5d0f3c946c344c00fe421757f9efd22ea6e58817
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-09-19 03:44:45 -0600

    devices/stm32h7: Add support for the H7 PWR device.

--- psdb/devices/stm32h7/__init__.py
+++ psdb/devices/stm32h7/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2020 Phase Advanced Sensor Systems, Inc.
 from .flash_dp import FLASH_DP
 from .flash_up import FLASH_UP
+from .pwr import PWR
 
 
 __all__ = ['FLASH_DP',
            'FLASH_UP',
+           'PWR',
            ]
