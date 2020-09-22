fa4fae1036720164ec14600d57e2c20cebb5fcde
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-09-22 15:24:33 -0600

    stm32h7/tim15.py: Add support for TIM15 device.

--- psdb/devices/stm32h7/__init__.py
+++ psdb/devices/stm32h7/__init__.py
@@ -3,10 +3,12 @@ from .flash_dp import FLASH_DP
 from .flash_up import FLASH_UP
 from .pwr import PWR
 from .rcc import RCC
+from .tim15 import TIM15
 
 
 __all__ = ['FLASH_DP',
            'FLASH_UP',
            'PWR',
            'RCC',
+           'TIM15',
            ]
