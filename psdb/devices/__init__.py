def66665f755332fb2b4b27ff7f1db65ca10a6e6
Terry Greeniaus <terrygreeniaus@gmail.com>
2020-12-11 16:29:47 -0800

    devices/device.py: Add AReg types that use absolute bit positions instead of ordered lengths.

--- psdb/devices/__init__.py
+++ psdb/devices/__init__.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2020 Phase Advanced Sensor Systems, Inc.
-from .device import (Device, Reg, Reg32, Reg32R, Reg32W, Reg32RS, RegDiv,
-                     MemDevice, RAMDevice)
+from .device import (Device, Reg, Reg32, Reg32R, Reg32W, Reg32RS, AReg32,
+                     AReg32R, AReg32W, AReg32RS, RegDiv, MemDevice, RAMDevice)
 from .flash import Flash
 from . import core
 
 
-__all__ = ['Device',
+__all__ = ['AReg32',
+           'AReg32R',
+           'AReg32W',
+           'AReg32RS',
+           'Device',
            'Flash',
            'MemDevice',
            'RAMDevice',
