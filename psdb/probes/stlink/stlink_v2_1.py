c0c1d2e7f5afc71643b9fd64ca1a7c5c7f6ec699
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-06-24 00:58:17 -0700

    stlink/stlink_v2_1.py: Fix versioning for scatter-gather availability.

--- psdb/probes/stlink/stlink_v2_1.py
+++ psdb/probes/stlink/stlink_v2_1.py
@@ -38,9 +38,10 @@ class STLinkV2_1(stlink.STLink):
         if self.ver_jtag >= 26:
             self.features |= stlink.FEATURE_BULK_READ_16
             self.features |= stlink.FEATURE_BULK_WRITE_16
-            self.features |= stlink.FEATURE_SCATTERGATHER
         if self.ver_jtag >= 28:
             self.features |= stlink.FEATURE_OPEN_AP
+        if self.ver_jtag >= 32:
+            self.features |= stlink.FEATURE_SCATTERGATHER
 
     def _check_xfer_status(self):
         if self.features & stlink.FEATURE_RW_STATUS_12:
