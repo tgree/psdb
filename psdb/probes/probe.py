6f71aa622a2abb3b59a8ba9925f3b60e9bead873
Terry Greeniaus <terrygreeniaus@gmail.com>
2022-06-24 00:58:17 -0700

    probes/probe.py: Add get_stats() API.

--- psdb/probes/probe.py
+++ psdb/probes/probe.py
@@ -18,6 +18,11 @@ class Enumeration:
         raise NotImplementedError
 
 
+class Stats:
+    def dump(self):
+        print("Statistics not supported.")
+
+
 class Probe(object):
     def __init__(self):
         self.aps    = {}
@@ -33,6 +38,12 @@ class Probe(object):
 
     def set_tck_freq(self, freq):
         raise NotImplementedError
+
+    def get_stats(self):
+        '''
+        Return accumulated stats since the last time get_stats was invoked.
+        '''
+        return Stats()
 
     def open_ap(self, ap_num):
         raise NotImplementedError
