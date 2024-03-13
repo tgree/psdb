182a077a5587b97878116165821ac269edc4b2df
Terry Greeniaus <terrygreeniaus@gmail.com>
2024-03-13 17:22:32 -0600

    target/__init__.py: Sort targets by name.

--- psdb/targets/__init__.py
+++ psdb/targets/__init__.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2018-2019 Phase Advanced Sensor Systems, Inc.
 from .target import (Target, MemRegion)
 from . import msp432
-from . import stm32h7
 from . import stm32g0
 from . import stm32g4
-from . import stm32wb55
+from . import stm32h7
 from . import stm32u5
+from . import stm32wb55
 
 
 __all__ = ['Target',
@@ -13,12 +13,12 @@ __all__ = ['Target',
            ]
 
 TARGETS = [msp432.MSP432P401,
-           stm32h7.STM32H7,
-           stm32h7.STM32H7_DP,
            stm32g0.STM32G0,
            stm32g4.STM32G4,
-           stm32wb55.STM32WB55,
+           stm32h7.STM32H7,
+           stm32h7.STM32H7_DP,
            stm32u5.STM32U5,
+           stm32wb55.STM32WB55,
            ]
 
 
