f2a68759989363bf36191525439063f0c5a29743
Terry Greeniaus <terrygreeniaus@gmail.com>
2024-04-09 17:24:28 -0600

    target/stm32c0: Add support for RCC peripheral.

--- psdb/devices/stm32c0/__init__.py
+++ psdb/devices/stm32c0/__init__.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2024 Phase Advanced Sensor Systems, Inc.
 from .flash import FLASH
 from .pwr import PWR
+from .rcc import RCC
 
 
 __all__ = ['FLASH',
            'PWR',
+           'RCC',
            ]
--- psdb/targets/stm32c0/stm32c0.py
+++ psdb/targets/stm32c0/stm32c0.py
@@ -6,6 +6,7 @@ from . import dbgmcu
 
 
 DEVICES = [(RAMDevice,      'SRAM',     0x20000000, 0x00003000),
+           (stm32c0.RCC,    'RCC',      0x40021000),
            (stm32c0.FLASH,  'FLASH',    0x40022000, 0x08000000, 4000000,
                                         0x1FFF7000, 1024),  # noqa: E127
            (stm32c0.PWR,    'PWR',      0x40007000),
