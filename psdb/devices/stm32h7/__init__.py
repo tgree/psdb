dfa86bc95bef0e2a470ebdd70147ecabd944c6d5
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-01-08 11:32:41 -0700

    devices/stm32h7: Add support for STM32H72x/3x PWR, RCC and SPI peripherals.

--- psdb/devices/stm32h7/__init__.py
+++ psdb/devices/stm32h7/__init__.py
@@ -3,7 +3,9 @@ from .flash_2x_3x import FLASH_2x_3x
 from .flash_dp import FLASH_DP
 from .flash_up import FLASH_UP
 from .pwr import PWR
+from .pwr_2x_3x import PWR_2x_3x
 from .rcc import RCC
+from .rcc_2x_3x import RCC_2x_3x
 from .tim6 import TIM6
 from .tim15 import TIM15
 from .tim17 import TIM17
@@ -11,6 +13,7 @@ from .tim2_5 import TIM2_5
 from .syscfg import SYSCFG
 from .art import ART
 from .opamp import OPAMP
+from .spi import SPI
 
 TIM7 = TIM6
 
@@ -20,7 +23,9 @@ __all__ = ['ART',
            'FLASH_DP',
            'FLASH_UP',
            'PWR',
+           'PWR_2x_3x',
            'RCC',
+           'RCC_2x_3x',
            'TIM6',
            'TIM7',
            'TIM15',
@@ -28,4 +33,5 @@ __all__ = ['ART',
            'TIM2_5',
            'SYSCFG',
            'OPAMP',
+           'SPI',
            ]
